ae8a4f82131da3999db38abd6538a840faa80a7d
10b2 <10b2@pm.me>
2018-12-03 13:15:06 -0600

    fix: tmux plugins move to XDG_DATA_HOME/tmux/plugins

--- .config/profile/99-tmux.sh
+++ .config/profile/99-tmux.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 #  Location for TMUX PLUGIN MANAGER
-set_env_var "TMUX_PLUGIN_MANAGER_PATH" "${XDG_CONFIG_HOME}/tmux/plugins/tpm"
+set_env_var "TMUX_PLUGIN_MANAGER_PATH" "${XDG_DATA_HOME}/tmux/plugins/"
 
 #   # Check if current shell is running inside TMUX
 #   if [ -z "${TMUX:-}" ]; then
