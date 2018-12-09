275efc0e459811e33c0121f57d18bfcc165b473c
10b2 <10b2@pm.me>
2018-12-08 21:11:50 -0600

    chore: NVIM_DIR to XDG_DATA_HOME/nvm

--- .config/profile/40-node.sh
+++ .config/profile/40-node.sh
@@ -8,6 +8,10 @@ if [ ! -d "${XDG_DATA_HOME}/npm" ]; then
     mkdir "${XDG_DATA_HOME}/npm" 
 fi
 
+if [ ! -d "${XDG_DATA_HOME}/nvm" ]; then
+    mkdir "${XDG_DATA_HOME}/nvm"
+fi
+
 if [ ! -d "${XDG_CACHE_HOME}/npm" ]; then
     mkdir "${XDG_CACHE_HOME}/npm" 
 fi
@@ -15,3 +19,4 @@ fi
 set_env_var "NPM_CONFIG_USERCONFIG" "${XDG_CONFIG_HOME}/npm/npmrc"
 set_env_var "NPM_CONFIG_INIT_MODULE" "${XDG_DATA_HOME}/npm/npm-init.js"
 set_env_var "NPM_CONFIG_CACHE" "${XDG_CACHE_HOME}/npm"
+set_env_var "NVM_DIR" "${XDG_DATA_HOME}/nvm"
